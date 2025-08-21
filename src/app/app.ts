85869d9e38fb5612e921ecd2758305de7c55eff2
Alec Custer <alcuster@icloud.com>
2025-08-21 09:04:50 -0700

    add things

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,24 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgxFlickingComponent } from '@egjs/ngx-flicking';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgxFlickingComponent],
   template: `
     <h1>Welcome to {{ title() }}!</h1>
-
+    <ngx-flicking class="flicking w-[500px]" [options]="{ circular: true, moveType: 'freeScroll' }">
+      <div class="mr-2 h-[200px] w-[200px] bg-blue-400"></div>
+      <div class="mr-2 h-[200px] w-[200px] bg-blue-400"></div>
+      <div class="mr-2 h-[200px] w-[200px] bg-blue-400"></div>
+      <div class="mr-2 h-[200px] w-[200px] bg-blue-400"></div>
+      <div class="mr-2 h-[200px] w-[200px] bg-blue-400"></div>
+      <div class="mr-2 h-[200px] w-[200px] bg-blue-400"></div>
+    </ngx-flicking>
     <router-outlet />
   `,
   styles: [],
 })
 export class App {
-  protected readonly title = signal('my-flicking-app');
+  protected readonly title = signal('ngx-flicking-bug');
 }
